87112daac2ad907a8a6b98a7648d5f8692ae8e30
Brilian Ade Putra <brilianadeputra@gmail.com>
2023-07-19 16:37:56 +0700

    Initial Commit

---
+++ The Notes/The_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_NotesApp.swift
+//  The Notes
+//
+//  Created by Brilian Ade Putra on 19/07/23.
+//
+
+import SwiftUI
+
+@main
+struct The_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
